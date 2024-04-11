588002e77b1caef0ac15fb0957d93fdf668376b2
Anna Waterhouse <annawaterhouse05@gmail.com>
2024-04-11 15:23:31 -0400

    layout reset

---
+++ src/client/layout/Root.jsx
@@ -0,0 +1,10 @@
+export default function Root() {
+    return (
+        <section className="container grid">
+            <h1>Nav</h1>
+            <main>
+
+            </main>
+        </section>
+    )
+}
---
+++ src/client/main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Root from "./layout/Root";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+    //   { path: "/", element: <Tasks /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+      <RouterProvider router={router} />
+  </React.StrictMode>
+);
